4adbf9fc2561e44c1fb643a4648975b2f94f1591
Arlandaren <bad-07@list.ru>
2024-02-29 02:41:48 +0900

    country fix

--- solution/api/country.go
+++ solution/api/country.go
@@ -4,6 +4,12 @@ import(
 	"github.com/gin-gonic/gin"
 	"solution/models"
 )
+type CountryResponse struct {
+    Name   string `json:"name"`
+    Alpha2 string `json:"alpha2"`
+    Alpha3 string `json:"alpha3"`
+    Region string `json:"region"`
+}
 
 func GetAllCountries(c *gin.Context){
 	region := c.Query("region")
@@ -17,10 +23,17 @@ func GetAllCountries(c *gin.Context){
 func GetCountryByid(c *gin.Context){
 	alpha2 := c.Param("alpha2")
 	country, err := models.GetCountryByid(alpha2)
+	response := CountryResponse{
+        Name:   country.Name,
+        Alpha2: country.Alpha2,
+        Alpha3: country.Alpha3,
+        Region: country.Region,
+    }
+
 	if err != nil{
 		c.JSON(500, gin.H{"error":err.Error()})
 	} else{
-		c.JSON(200,country)
+		c.JSON(200,response)
 	}
 
 }
