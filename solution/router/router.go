db530119604bf7f77eb9978656b1b5ca5dead05b
Arlandaren <bad-07@list.ru>
2024-02-29 01:17:01 +0900

    router fix

--- solution/router/router.go
+++ solution/router/router.go
@@ -9,8 +9,8 @@ import (
 func RouteAll(r * gin.Engine){
 	api_router := r.Group("api")
 	{
-		api_router.Use(middlewares.AuthValidation)
 		api_router.GET("/ping", func(c *gin.Context){c.String(200, "pong")})
+		api_router.Use(middlewares.AuthValidation)
 		api_router.GET("/countries", api.GetAllCountries)
 		api_router.GET("/countries/:alpha2", api.GetCountryByid)
 	}
