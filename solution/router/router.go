90d8cef1ddc4924a8878cce6129ebf3232948215
Arlandaren <bad-07@list.ru>
2024-02-29 01:34:02 +0900

    router

--- solution/router/router.go
+++ solution/router/router.go
@@ -13,10 +13,11 @@ func RouteAll(r * gin.Engine){
 		api_router.GET("/countries", api.GetAllCountries)
 		api_router.GET("/countries/:alpha2", api.GetCountryByid)
 		// api_router.Use(middlewares.AuthValidation)
-	}
-	auth := r.Group("auth")
-	{
+		auth := r.Group("auth")
+		{
 		auth.POST("/sign-in", api.Signin)
 		auth.POST("/register",api.Register)
+		}
 	}
+	
 }
