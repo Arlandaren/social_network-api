95897e7dd2616404021e49d6a85e1fd802b22e53
Arlandaren <bad-07@list.ru>
2024-03-02 03:49:56 +0900

    register fix

--- solution/pkg/models/db.go
+++ solution/pkg/models/db.go
@@ -161,7 +161,7 @@ func GetUser(username string) (*User, error) {
 	}
 	return &user, nil
 }
-func CreateUser(username string, email string, password string, country string, is_public bool, phone_number string, image string) (map[string]interface{},error) { 
+func CreateUser(username string, email string, password string, country string, is_public bool, phone_number string, image string) (*Profile,error) { 
 	if username == "" || email == "" || password == "" || country == ""{
         return nil, errors.New("неверный формат")
     }
@@ -169,15 +169,24 @@ func CreateUser(username string, email string, password string, country string, 
     if err != nil {
         return nil,err
     }
-	profile := map[string]interface{}{
-		"login": username,
-		"email": email,
-		"countryCode": country,
-		"isPublic": is_public,
-		"phone": phone_number,
-		"image": image,
+	// profile := map[string]interface{}{
+	// 	"login": username,
+	// 	"email": email,
+	// 	"countryCode": country,
+	// 	"isPublic": is_public,
+	// 	"phone": phone_number,
+	// 	"image": image,
+	// }
+	profile := Profile{
+		Login     : username,
+		Email   : email,
+		CountryCode :country,
+		IsPublic : is_public,
+		Phone : phone_number,
+		Image : image,
+		
 	}
-    return profile, nil
+    return &profile, nil
 }
 func GetMyProfile(id uint)(*Profile, error){
 	var profile Profile
